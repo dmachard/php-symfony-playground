11219d91b1c22f155a7b7cc8551d1a0885aaa89e
dmachard <5562930+dmachard@users.noreply.github.com>
2025-11-03 14:37:49 +0100

    backend: add delete link

--- backend/src/Entity/Link.php
+++ backend/src/Entity/Link.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
 use App\Repository\LinkRepository;
 use App\State\LinkProvider;
 use Doctrine\ORM\Mapping as ORM;
@@ -21,6 +22,10 @@ use Doctrine\ORM\Mapping as ORM;
             security: "is_granted('ROLE_USER')",
             securityPostDenormalize: "is_granted('ROLE_ADMIN') or object.getUser() == null or object.getUser().getEmail() == user.getUserIdentifier()",
             securityPostDenormalizeMessage: "You can only create links for yourself unless you are admin."
+        ),
+        new Delete(
+            security: "is_granted('ROLE_ADMIN') or object.getUser().getEmail() == user.getUserIdentifier()",
+            securityMessage: "You can only delete your own links unless you are an admin."
         )
     ]
 )]
